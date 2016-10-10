e961eba2ef365b554aa9b5e4ad04b2b34b1d541c
AntonDolgov <dlgvnt@gmail.com>
2016-10-11 02:21:25 +0300

    Задача на преобразование объектов в примитивы

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-function filter (arr, func) {
-	var newArray = [];
-	for(var i=0;i<arr.length; i++) {
-		if (func(arr[i])) {
-			newArray.push(arr[i]);
-		}
-	}
-	return newArray;
+function sum(a) {
+
+  var currentSum = a;
+
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+
+  return f;
 }
 
-function inBetween(a,b) {
-	return function (x) {
-		return x>=a && x<=b;
-	}		
-}
-
-function inArray(arr) {
-	var result = false;
-	function (x) {
-		for (var i=0; i<arr.length; i++) {
-			if (x==arr[i]) {
-				result = true;
-			}
-		}		
-	}
-	return result;
-}
-
-var arr = [1, 2, 3, 4, 5, 6, 7];
-
-alert(filter(arr, function(a) {
-  return a % 2 == 0
-})); // 2,4,6
-
-alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
-alert( filter(arr, inArray([1, 2, 10])) ); // 1,2
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1)(2) ); // 6
+alert( sum(6)(-1)(-2)(-3) ); // 0
+alert( sum(0)(1)(2)(3)(4)(5) ); // 15
