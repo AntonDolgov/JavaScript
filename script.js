117bf83298260eaf559bc9cd7a506ae054f50044
AntonDolgov <dlgvnt@gmail.com>
2016-10-12 01:36:30 +0300

    Задача на методы call и apply

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 /*
-Добавить в конструктор Article:
+Напишите функцию applyAll(func, arg1, arg2...), 
+которая получает функцию func и произвольное 
+количество аргументов.
 
-Подсчёт общего количества созданных объектов.
-Запоминание даты последнего созданного объекта.
-Используйте для этого статические свойства.
-
-Пусть вызов Article.showStats() выводит то и другое.
-
-Использование:
+Она должна вызвать func(arg1, arg2...), то есть 
+передать в func все аргументы, начиная со второго, 
+и возвратить результат.
 */
 
-function Article() {
-  this.created = new Date();
-  if (Article.count == undefined) {
-  	Article.count = 0;
-  }
-  
-  Article.count++;
-  Article.date = this.created;
+function applyAll(arr) {
+	/* конкретно в данном задании контекст метода 
+	apply не нужен. "this" используется на всякий 
+	случай. Специально в самом низу добавил этот случай
+	в качестве примера
+	*/
+	return arr.apply(this, [].slice.call(arguments,1));
 }
 
-Article.showStats = function() {
-	alert('Всего:'+ this.count + ', Последняя: ' + this.date);
+// Применить Math.max к аргументам 2, -2, 3
+alert( applyAll(Math.max, 2, -2, 3) ); // 3
+
+// Применить Math.min к аргументам 2, -2, 3
+alert( applyAll(Math.min, 2, -2, 3) ); // -2
+
+
+function sum() { // суммирует аргументы 
+  return [].reduce.call(arguments, function(a, b) {
+    return a + b;
+  });
 }
 
-new Article();
-new Article();
+function mul() { // перемножает аргументы
+  return [].reduce.call(arguments, function(a, b) {
+    return a * b;
+  });
+}
 
-Article.showStats(); // Всего: 2, Последняя: (дата)
+alert( applyAll(sum, 1, 2, 3) ); // sum(1, 2, 3) = 6
+alert( applyAll(mul, 2, 3, 4) ); // mul(2, 3, 4) = 24
 
-new Article();
+//в качестве примера, когда "this" пригодится:
+var obj = {
+    i: 10,
+    f: function (arg) {
+        alert(this.i + arg)
+    }
+};
 
-Article.showStats(); // Всего: 3, Последняя: (дата)
+obj.applyAll = applyAll; // "одолжили" метод
+obj.applyAll(obj.f, 10); // 20
