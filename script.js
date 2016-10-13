bc01220566df66bec322a43a422638411056a998
AntonDolgov <dlgvnt@gmail.com>
2016-10-13 04:36:55 +0300

    Задачка на тему "геттеры и сеттеры"

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
 /*
-Улучшите готовый код кофеварки, который дан ниже: добавьте в кофеварку 
-публичный метод stop(), который будет останавливать 
-кипячение (через clearTimeout).
+Обычно когда кофе готов, мы хотим что-то сделать, например выпить его.
+Сейчас при готовности срабатывает функция onReady, но она жёстко задана в коде:
 */
 
-
-function CoffeeMachine(power) {
-  this.waterAmount = 0;
+function CoffeeMachine(power, capacity) {
+  var waterAmount = 0;
 
   var WATER_HEAT_CAPACITY = 4200;
 
-  var self = this;
-
-  function getBoilTime() {
-    return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+  function getTimeToBoil() {
+    return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
   }
 
-  function onReady() {
-    alert( 'Кофе готово!' );
-  }
-  
-  var timerId;
-  this.run = function() {
-    timerId = setTimeout(onReady, getBoilTime());
-    return timerId;
+  this.setWaterAmount = function(amount) {
+    // ... проверки пропущены для краткости
+    waterAmount = amount;
   };
 
-  this.stop = function () {
-    clearTimeout(timerId);
+  this.getWaterAmount = function(amount) {
+    return waterAmount;
+  };
+
+  function onReady() {
+      alert( 'Кофе готов!' );
+  }
+
+  this.setOnReady = function(func) {
+    onReady = function () {
+      return func();
+    }
+  }
+
+  this.run = function() {
+    setTimeout(onReady, getTimeToBoil());
   };
 
 }
 
+//Создайте сеттер setOnReady, чтобы код снаружи мог назначить свой onReady, вот так:
 
-var coffeeMachine = new CoffeeMachine(50000);
-coffeeMachine.waterAmount = 200;
+var coffeeMachine = new CoffeeMachine(50000, 500);
+coffeeMachine.setWaterAmount(150);
+
+coffeeMachine.setOnReady(function() {
+  var amount = coffeeMachine.getWaterAmount();
+  alert( 'Готов кофе: ' + amount + 'мл' ); // Кофе готов: 150 мл
+});
 
 coffeeMachine.run();
-coffeeMachine.stop(); // кофе приготовлен не будет
+
+coffeeMachine.setOnReady(function() {
+  var amount = coffeeMachine.getWaterAmount();
+  alert( 'Готов кофе: ' + amount + 'мsdfsdfл' ); // Кофе готов: 150 мл
+});
+
+coffeeMachine.run();
+
+/*
+P.S. Значение onReady по умолчанию должно быть таким же, как и раньше.
+
+P.P.S. Постарайтесь сделать так, чтобы setOnReady можно было вызвать не 
+только до, но и после запуска кофеварки, то есть чтобы функцию onReady 
+можно было изменить в любой момент до её срабатывания.
+
+!!! Мой метод решения оказался даже более читабельным (на мой взгляд),
+чем решение, предложенное авторами. Я изменил код в одной строчке, в то
+время как им пришлось изменять две функции. Их решение:
+
+this.setOnReady = function(newOnReady) {
+    onReady = newOnReady;
+  };
+
+  this.run = function() {
+    setTimeout(function() {
+      onReady();
+    }, getTimeToBoil());
+  };
+
+*/
